8d45a565d45c59d5cb1877856a00b5cd3fedb5c0
Isma3ilovitch <ismailtaboubi7@gmail.com>
2025-08-06 22:58:42 +0100

    Update constants.dart

--- lib/ utils/constants.dart
+++ lib/ utils/constants.dart
@@ -1,8 +1,4 @@
 class AppConstants {
   static const String appName = 'Warranty Keeper';
-  static const String hiveBoxName = 'invoices';
-  static const String notificationChannelId = 'warranty_expiry_channel';
-  static const String notificationChannelName = 'Warranty Expiry Notifications';
-  static const String notificationChannelDescription =
-      'Notifications for warranty expiry';
+  static const String prefInvoiceKey = 'saved_invoices_list';
 }
